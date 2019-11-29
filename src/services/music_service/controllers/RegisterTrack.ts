56894e63666722300962f1d6c06e2ab43a8435b9
tototidan <alexis.broustaut@ynov.com>
2019-11-29 15:20:41 +0100

    feature: track register

--- src/services/music_service/controllers/RegisterTrack.ts
+++ src/services/music_service/controllers/RegisterTrack.ts
@@ -4,19 +4,23 @@ import { getMongoRepository } from 'typeorm';
 
 export function registerTracks(message : Message)
 {
+    console.log("ok2")
     let info = JSON.parse(message.getContent());
     let track = new Track();
     try
     {
-        track.artist = info.artist;
+        track.owner = info.owner;
         track.cover = info.cover;
         track.genre = info.genre;
         track.link = info.link;
         getMongoRepository(Track).save(track);
+        return track.toJson();
+
     }
     catch(e)
     {
-
+        console.log("ok");
+        return "tamere"
     }
      
     return "ok";
